65d57554f7c6cca7e8c9ed6e7456d421ca898b23
VladislavPonomarenko <vladponomarenko8@gmail.com>
2017-03-01 12:46:11 +0200

    0103171245

--- app/src/main/java/com/example/scarlet/englishgrammartest/PastTime/PastSimple.java
+++ app/src/main/java/com/example/scarlet/englishgrammartest/PastTime/PastSimple.java
@@ -1,13 +1,34 @@
 package com.example.scarlet.englishgrammartest.PastTime;
 import com.example.scarlet.englishgrammartest.R;
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class PastSimple extends AppCompatActivity {
+public class PastSimple extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnGoPastSimpleTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_past_simple);
+
+        btnGoPastSimpleTest=(Button) findViewById(R.id.btnGoPastSimpleTest);
+        btnGoPastSimpleTest.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnGoPastSimpleTest:
+                Intent intent = new Intent(this,PastSimpleTest.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/scarlet/englishgrammartest/PastTime/PastSimpleTest.java
@@ -0,0 +1,14 @@
+package com.example.scarlet.englishgrammartest.PastTime;
+import com.example.scarlet.englishgrammartest.R;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PastSimpleTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_past_simple_test);
+    }
+}
