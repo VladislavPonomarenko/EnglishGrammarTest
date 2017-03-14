a857db1a1b139c6e44cb1cdc92049c19ec4ca2b1
VladislavPonomarenko <vladponomarenko8@gmail.com>
2017-03-15 00:53:17 +0200

    1503170053

---
+++ app/src/main/java/com/example/scarlet/englishgrammartest/PastTime/PastContinuousTest.java
@@ -0,0 +1,15 @@
+package com.example.scarlet.englishgrammartest.PastTime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.scarlet.englishgrammartest.R;
+
+public class PastContinuousTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_past_continuous_test);
+    }
+}
---
+++ app/src/main/java/com/example/scarlet/englishgrammartest/PastTime/PastPerfectTest.java
@@ -0,0 +1,15 @@
+package com.example.scarlet.englishgrammartest.PastTime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.scarlet.englishgrammartest.R;
+
+public class PastPerfectTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_past_perfect_test);
+    }
+}
